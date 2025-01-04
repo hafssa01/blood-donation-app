c9868d54f5dfab8650eb3ff67078da60db4099d2
hafssa01 <hafssazaferr@gmail.com>
2025-01-04 15:48:49 +0100

    Adding bundel.js

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { User, LogIn, LogOut, Heart, UserPlus, Share2 } from 'lucide-react';  // Add necessary icons
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import logo from '../assets/logo.png';  // Adjust path if necessary
 
 const NavigationBar = ({ isLoggedIn, onLogout }) => {
